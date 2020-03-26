d4c9db59dd4de646fc6acccdb691314631d2b977
Mariana Lima <m.budiski@gmail.com>
2020-03-26 19:31:07 -0300

    ajustando tela de registro de usuários

--- src/views/Registry.vue
+++ src/views/Registry.vue
@@ -1,91 +1,31 @@
 <template>
-  <div class="container-fluid home">
+  <div class="container-fluid registry">
     <div class="row justify-content-md-center">
-      <div class="col-lg-3 col-md-6">
-        <h3>Novo Sorteio</h3>
+      <div class="col-lg-5 col-md-6 col-sm-12 col-12">
+        <h3>Entrar em um Jogo</h3>
         <div class="form-group">
+          <label>Informe seu nome:</label>
           <input
             type="text"
             class="form-control text-center"
-            id="description"
-            v-model="description"
-            aria-describedby="descriptionHelp"
-            placeholder="Informe a descrição do sorteio">
+            id="name"
+            v-model="name"
+            aria-describedby="nameHelp"
+          />
           <small
-            v-if="!description"
-            id="descriptionHelp"
+            v-if="!name"
+            id="nameHelp"
             class="form-text text-muted text-danger"
             >Campo obrigatório.</small
           >
         </div>
-      </div>
-    </div>
-    <br />
-    <div class="row justify-content-md-center">
-      <div class="col-lg-12 col-md-12 col-sm-12 col-12">
-        <h3>Nomes</h3>
-      </div>
-      <div class="col-lg-2 col-md-5 col-sm-8 col-8">
-        <div class="form-group">
-          <input
-            type="text"
-            class="form-control"
-            id="name"
-            v-model="newName"
-            aria-describedby="nameHelp"
-            placeholder="Informe um novo nome"
-          />
-        </div>
-      </div>
-      <div class="col-lg-1 col-md-3 col-sm-4 col-4">
-        <button type="button" class="btn btn-app" v-on:click="add(index)">
-          Adicionar
-        </button>
-      </div>
-    </div>
-    <div class="row justify-content-md-center">
-      <div class="col-lg-3 col-md-8 col-sm-12 col-12">
-        <div class="alert alert-warning" role="alert" v-if="!names.length">
-          Informe alguns nomes para sortear
-        </div>
-        <table class="table table-striped table-hover" v-if="names.length">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Nome</th>
-              <th scope="col"></th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="(name, index) in names" v-bind:key="index">
-              <th scope="row">
-                {{ index + 1 }}
-              </th>
-              <td>
-                {{ name }}
-              </td>
-              <td>
-                <span
-                  class="cursor-pointer text-danger"
-                  v-on:click="remove(index)"
-                >
-                  <font-awesome-icon icon="trash-alt" />
-                </span>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
-    <div class="row justify-content-md-center">
-      <div class="col-lg-3 col-md-6">
         <button
           type="button"
           class="btn btn-app btn-ruffle"
-          v-on:click="ruffle"
-          :disabled="!names.length || !description"
+          v-on:click="login"
+          :disabled="!name"
         >
-          Sortear!
+          Entrar
         </button>
       </div>
     </div>
@@ -94,45 +34,18 @@
 
 <script>
 export default {
-  name: "Home",
+  name: "Registry",
   data() {
     return {
-      description: "",
-      names: [],
-      newName: ""
+      name: ""
     };
   },
   methods: {
-    ruffle() {
-      var index = Math.floor(Math.random() * this.names.length);
-      var drawn = this.names[index];
+    login() {
       this.$router.push({
-        name: "Ruffle",
-        params: {
-          description: this.description,
-          names: this.names,
-          drawn: drawn
-        }
+        name: "Game"
       });
-    },
-    add() {
-      if (this.newName) {
-        this.names.push(this.newName);
-      }
-    },
-    remove(index) {
-      this.names.splice(index, 1);
     }
-  },
-  mounted() {
-    if (this.$route.params.names && this.$route.params.description) {
-      this.description = this.$route.params.description;
-      this.names = this.$route.params.names;
-    } else {
-      this.description = "Basal da Semana";
-      this.names = ["Hugo", "Leo", "Mari", "Naty", "Vini"];
-    }
-    this.newName = "";
   }
 };
 </script>
